da873cbe097b2a823691bbfada3db8d150b6eca9
CanKirsallioba <cankirsallioba@gmail.com>
2023-04-06 11:57:34 +0100

    fixed and completed bm25

--- bm25.py
+++ bm25.py
@@ -1,94 +1,67 @@
 import pandas as pd
-import numpy as np
-import re
 import nltk
-from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from tqdm import tqdm
+import math
 
-# Load the metadata.csv file using pandas
-metadata = pd.read_csv("metadata.csv", dtype={"abstract": str}, low_memory=False)
+nltk.download('punkt')
 
-# Define the BM25 parameters
-k1 = 1.2
-b = 0.75
+# Read metadata.csv and use only the first 10,000 rows
+df = pd.read_csv('metadata.csv', nrows=10000)
 
-# Preprocess the text data
-def preprocess_text(text):
-    if isinstance(text, str):
-        # Convert to lowercase
-        text = text.lower()
-        # Remove punctuations
-        text = re.sub(r'[^\w\s]','',text)
-        # Tokenize the text
-        tokens = nltk.word_tokenize(text)
-        # Remove stop words
-        stop_words = set(stopwords.words('english'))
-        tokens = [token for token in tokens if token not in stop_words]
-        # Stem the words
-        stemmer = SnowballStemmer("english")
-        tokens = [stemmer.stem(token) for token in tqdm(tokens)]
-        # Join the tokens back into a string
-        text = " ".join(tokens)
+# Preprocess the text
+def preprocess(text):
+    return nltk.word_tokenize(text.lower())
+
+# Tokenize the title and abstract columns
+tokenized_corpus = []
+for index, row in df.iterrows():
+    if type(row['title']) == str:
+        title_tokens = preprocess(row['title'])
     else:
-        text = ""
-    return text
+        title_tokens = []
 
-metadata['processed_text'] = metadata['abstract'].apply(preprocess_text)
+    if type(row['abstract']) == str:
+        abstract_tokens = preprocess(row['abstract'])
+    else:
+        abstract_tokens = []
 
-print('After preprocessing ...')
+    tokenized_corpus.append(title_tokens + abstract_tokens)
 
-# Create the document-term matrix
-count_vect = CountVectorizer()
-doc_term_matrix = count_vect.fit_transform(metadata['processed_text'])
+# BM25 Implementation
+def idf(token, tokenized_corpus):
+    N = len(tokenized_corpus)
+    n_q = sum(1 for doc in tokenized_corpus if token in doc)
+    return math.log((N - n_q + 0.5) / (n_q + 0.5))
 
-# Compute the IDF values
-tfidf_transformer = TfidfTransformer()
-tfidf_transformer.fit(doc_term_matrix)
-idf = tfidf_transformer.idf_
+def bm25(query_tokens, doc_tokens, tokenized_corpus, k1=1.5, b=0.75):
+    doc_len = len(doc_tokens)
+    avg_doc_len = sum(len(doc) for doc in tokenized_corpus) / len(tokenized_corpus)
+    score = 0
 
-# Compute the document length
-doc_len = np.sum(doc_term_matrix, axis=1)
+    for token in query_tokens:
+        if token not in doc_tokens:
+            continue
 
-# Compute the average document length
-avg_doc_len = np.mean(doc_len)
-
-# Compute the BM25 score for each document
-def compute_bm25_score(query, doc_idx):
-    # Get the document length
-    dl = doc_len[doc_idx]
-    # Convert the query to a document-term matrix
-    query_matrix = count_vect.transform([query])
-    # Compute the term frequency in the query
-    tf = query_matrix.sum(axis=1)
-    # Compute the document frequency for each term in the query
-    df = doc_term_matrix[:, query_matrix.indices].sum(axis=0)
-    # Compute the BM25 score for each term in the query
-    score = np.sum(np.log((metadata.shape[0] - df + 0.5) / (df + 0.5)) * (((k1 + 1) * tf) / ((k1 * ((1 - b) + b * (dl / avg_doc_len))) + tf)))
+        f_q = doc_tokens.count(token)
+        term_idf = idf(token, tokenized_corpus)
+        numerator = f_q * (k1 + 1)
+        denominator = f_q + k1 * (1 - b + b * (doc_len / avg_doc_len))
+        score += term_idf * (numerator / denominator)
 
     return score
 
+# Search function
+def search(query, tokenized_corpus, top_n=10):
+    query_tokens = preprocess(query)
+    scores = [bm25(query_tokens, doc_tokens, tokenized_corpus) for doc_tokens in tokenized_corpus]
+    top_indices = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)[:top_n]
+    return df.iloc[top_indices]
 
-# Define the query
-query = "COVID-19 pandemic"
+# Perform a query
+if __name__ == "__main__":
+    query = "COVID-19 transmission"
+    top_n = 10
+    results = search(query, tokenized_corpus, top_n)
 
-# Compute the BM25 score for each document
-bm25_scores = []
-for i in tqdm(range(metadata.shape[0])):
-    score = compute_bm25_score(query, i)
-    bm25_scores.append(score)
-
-# Sort the documents based on their BM25 scores
-idx_sorted = np.argsort(bm25_scores)[::-1]
-
-# Print the top-k documents with the highest BM25 scores
-k = 10
-for i in range(k):
-    idx = idx_sorted[i]
-    print("Title:", metadata.loc[idx, "title"])
-    print("Authors:", metadata.loc[idx, "authors"])
-    print("BM25 score:", bm25_scores[idx])
-    print("Abstract:", metadata.loc[idx, "abstract"])
-    print("\n")
+    print(f"Top {top_n} results for the query '{query}':")
+    for index, row in results.iterrows():
+        print(f"\nTitle: {row['title']}\nAbstract: {row['abstract']}\nURL: {row['url']}")
